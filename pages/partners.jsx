799d903fb25c3f35db6e62afaad2de7cfa395323
zenyrelax <159481756+zenyrelax@users.noreply.github.com>
2024-04-13 23:52:18 +0100

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -16,14 +16,14 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me">
+            <a href="https://discord.gg/bvrp">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/676124992919830548/72826a4ed7d1a010df263623be5dd4ba.png" />
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1228709600967135232/1228839391741612225/bvrpkesin.png?ex=662d80dd&is=661b0bdd&hm=101618fd068ac12e041eb29ddc33e8b38137cd8542cea521e9ab9f7b77223528&" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">BVRP</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                      Code Share, it offers an ideal platform for developers to share their projects, interact with other developers and sell their projects for money, exploring many projects in different categories and software languages.
+                                      FiveM Sunucusu Teması GUNRP Olan Bir Sunucu
                                     </p>
                                 </div>     
                             </div>
@@ -33,15 +33,14 @@ export default function Partners() {
 
 
                             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me/partners/64db583a4292ca1066ad0a1b">
+            <a href="https://discord.gg/verhex">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1172657853056684102/1172671829673005146/maskot_profil_alev.png" />
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1228709600967135232/1228839643655569600/Z-Logo.png?ex=662d8119&is=661b0c19&hm=ccd439ce4fb02d2c4314cacfb4af91881fd6035be3f09d168b665457b2130360&" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Nodesty VDS</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                   DISCOUNT CODE: CODESHARE Experience uninterrupted internet speed up to 10Gb and cheap intel i9, amd ryzen series vds with Nodesty. Shop with Code Share assurance.
-                                    </p>
+                                Vexhex Hile Yapan Bir Sunucudur Kendi Özel Hilenizi Oluşturmak İsterseniz Bu Sunucuya Girip Kendi Priv Hilenizi Alabilirsiniz
                                 </div>     
                             </div>
                         </div>
