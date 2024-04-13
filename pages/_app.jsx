52f4dc7bec01f53db789caf2e9d8e693bb61c222
zenyrelax <159481756+zenyrelax@users.noreply.github.com>
2024-04-13 23:39:27 +0100

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -44,7 +44,7 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Add Bot",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
-      href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands",
+      href: "https://discord.com/oauth2/authorize?client_id=1180966125077536819&permissions=8&scope=bot",
     },
        {
       link: true,
